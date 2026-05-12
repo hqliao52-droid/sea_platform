d4e9051ebe929d6ad88e1cf892ebdf4b37df7b11
hqliao52-droid <hqliao52@gmail.com>
2026-05-12 16:30:43 +0800

    1

--- app/tasks/scheduler.py
+++ app/tasks/scheduler.py
@@ -1,17 +1,17 @@
-from fastapi import APIRouter
-from app.utils.logger import Logger
-import threading
+# from fastapi import APIRouter
+# from app.utils.logger import Logger
+# import threading
 
-scheduler_task = APIRouter()
-logger = Logger.setup_logger(Logger.set_file_date())
+# scheduler_task = APIRouter()
+# logger = Logger.setup_logger(Logger.set_file_date())
 
-@scheduler_task.on_event("startup")
-def startup():
-    """启动时执行"""
-    from app.core.scheduler import scheduler
-    from app.tasks.crwal_task import spider_rss
-    if not scheduler.running:
-        scheduler.start()
+# @scheduler_task.on_event("startup")
+# def startup():
+#     """启动时执行"""
+#     from app.core.scheduler import scheduler
+#     from app.tasks.crwal_task import spider_rss
+#     if not scheduler.running:
+#         scheduler.start()
     
-    threading.Thread(target=spider_rss,daemon=True).start()
-    logger.info("定时任务启动成功！")
+#     threading.Thread(target=spider_rss,daemon=True).start()
+#     logger.info("定时任务启动成功！")
