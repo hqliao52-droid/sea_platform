3e252a90a94b7437d54b7b42e1a7e463314dce2e
hqliao52-droid <hqliao52@gmail.com>
2026-05-13 11:13:22 +0800

    1

--- app/api/rss_api.py
+++ app/api/rss_api.py
@@ -1,18 +1,20 @@
-from app.services.rss_service import RssSourceOperator
+# from app.services.rss_service import RssSourceOperator
 from app.utils.result_response import Result
 from fastapi import APIRouter,Query  
+from app.services.baidu_rss_service import BaiduRssSourceOperator
 
 router = APIRouter()
 
-rss = RssSourceOperator()
+# rss = RssSourceOperator()
+baidu_rss = BaiduRssSourceOperator()
 @router.get("/active_rss")
 def get_rss_source_list():
     """获取所有激活 rss 源"""
-    result = rss.get_active_rss_sources()
+    result = baidu_rss.get_active_rss_sources()
     return Result.success(data=result)
 
 @router.get("/get_by_url", summary="根据url获取rss源")
 async def get_by_url(url: str = Query(..., description="RSS源的URL地址")):
     """根据url获取rss源"""
-    result = rss.get_rss_detail_by_url(url)
+    result = baidu_rss.get_rss_detail_by_url(url)
     return Result.success(data=result)
